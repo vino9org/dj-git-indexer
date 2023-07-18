e02aa150271a1eceb1a819ff0084c93f9b6dea3d
Li Lin <guru.lin@gmail.com>
2023-07-18 18:50:43 +0800

    fix issue of default sqlite3 database file

--- crawler/settings.py
+++ crawler/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = "crawler.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / os.getenv("SQLITE_FILE", "db/git-indexer.db"),
+        "NAME": os.getenv("SQLITE_FILE", BASE_DIR / "db/git-indexer.db"),
     }
 }
 
