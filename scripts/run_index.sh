26f699a19244541b10fac0a7a16e306e61b7f56e
Li Lin <guru.lin@gmail.com>
2023-10-13 19:30:21 +0800

    udpate shell script

--- scripts/run_index.sh
+++ scripts/run_index.sh
@@ -3,24 +3,23 @@
 
 rotate_log()
 {
-log_file=$1
-max_logs=$2
+    log_file=$1
+    max_logs=$2
 
-# Rotate logs
-for (( i=$max_logs; i>=1; i-- )); do
-    if [[ -f "${log_file}.${i}" ]]; then
-        if (( i == $max_logs )); then
-            rm "${log_file}.${i}" # remove the oldest log
-        else
-            mv "${log_file}.${i}" "${log_file}.$((i+1))"
+    # Rotate logs
+    for (( i=$max_logs; i>=1; i-- )); do
+        if [[ -f "${log_file}.${i}" ]]; then
+            if (( i == $max_logs )); then
+                rm "${log_file}.${i}" # remove the oldest log
+            else
+                mv "${log_file}.${i}" "${log_file}.$((i+1))"
+            fi
         fi
+    done
+
+    if [ -f "${log_file}" ]; then
+    mv ${log_file} ${log_file}.1
     fi
-done
-
-if [ -f "${log_file}" ]; then
-   mv ${log_file} ${log_file}.1
-fi
-
 }
 
 RELPATH="$(dirname "$0")"/..
@@ -31,6 +30,16 @@ cd $WORK_DIR
 
 rotate_log logs/cron.log 3
 
-devbox run idx > logs/cron.log 2>&1
-devbox run ipr >> logs/cron.log 2>&1
+case $2 in
+    idx)
+        devbox run idx > logs/cron.log 2>&1
+        ;;
+    ipr)
+        devbox run ipr >> logs/cron.log 2>&1
+        ;;
+    *)
+        devbox run idx > logs/cron.log 2>&1
+        devbox run ipr >> logs/cron.log 2>&1
+        ;;
+esac
 
